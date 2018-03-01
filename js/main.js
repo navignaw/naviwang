de4b46e8e1b40986c92db8c3c911d77bf077849c
Ivan Wang <ivanw100@gmail.com>
2018-03-01 00:33:23 -0800

    add bounce animation to velociraptor

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
+const SmoothScroll = require('smooth-scroll');
 const Typed = require('typed.js');
+const clickHandler = require('click-handler');
 
+// Add class when raptor link is clicked.
+clickHandler.register('raptor', function(e) {
+  const raptorClasses = document.getElementById('raptor').classList;
+  raptorClasses.add('bounce');
+  document.getElementById('raptor').classList.add('bounce');
+  window.setTimeout(function() {
+    raptorClasses.remove('bounce');
+  }, 1000);
+  e.preventDefault();
+});
+
+// Add smooth scrolling to novel link.
+new SmoothScroll('a[href*="#novel"]');
+
+// Type out pills.
 const keywords = new Typed('.typed', {
   stringsElement: '#keywords',
   typeSpeed: 20,
